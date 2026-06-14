edaddb19f260885404015284f57a19f34fc11453
samuel <samusplay08@gmail.com>
2026-06-13 22:43:36 -0500

    Creadas clase modelos

---
+++ src/Servicies/Catalog/CatalogAPI/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace CatalogAPI.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    //Crea una nueva lisat ese new
+    public List<string> Category { get; set; } = new();
+
+    public string Description { get; set; } = default!;
+
+    public string ImageFile { get; set; } = default!;
+
+    public decimal Price { get; set; }
+
+
+
+}
+
---
+++ src/Servicies/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,7 @@
+﻿namespace CatalogAPI.Products.CreateProduct;
+
+public class CreateProductEndpoint
+{
+
+}
+
