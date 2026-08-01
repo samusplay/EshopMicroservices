d132c71770ead10bc0cb9c3910288d3e7857b367
samuel <samusplay08@gmail.com>
2026-07-31 20:41:01 -0500

    agregado hanlder y endpoint GetallProducts

---
+++ src/Servicies/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,42 @@
+﻿namespace CatalogAPI.Products.GetProducts;
+
+//como buena Practica Requerimos el record de Respuesta
+//Usamos record asi es mas facil hacer cambios a futuro
+//IEnumerbale es una coleccion que se puede recorrer
+//utilizamos el generic pra prometerle que solo hay entidades de Product
+public record GetProductResponse(IEnumerable<Product> Products);
+
+//Heredamos Icarter Module para definir los endpoints
+//con Carter no hay nececisidad de registra las rutas en el program.cs
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        //registramos el endpoint
+        //Regla cualquier cosa que haner con un servicio o DB externa es asincronica
+
+        //Isender es el mensajero hacia el Handler
+        //Usamos segregacion de interfaces utilizamos la herramientas Exacta
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            //mandamos la query al Hanlder con Send metodo de Isender
+            //llama al Handler por eso new GetProductQuery()
+            var result = await sender.Send(new GetProductQuery());
+
+            //retornamos la respuesta adaptandola segun los parametros
+            //separamos resposnabilidades
+            var response = result.Adapt<GetProductResponse>();
+
+            //lo devolvemos en Formato Json con las Minimal APIS
+            return Results.Ok(response);
+        })
+        // --- PROPIEDADES DEL ENDPOINT (SWAGGER) ---
+        .WithName("GetProducts")
+        .Produces<GetProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+
+    }
+}
+
---
+++ src/Servicies/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace CatalogAPI.Products.GetProducts;
+//1.Paso editar recors 
+//2.Paso crear handler y heredar interfza en inyectar dependencias
+//3.Crear Logica
+//Utilizamos Record porque no debe cambiar el mensaje
+//Usamos Generic para devolver el tipo de dato
+//Creamos una Firma
+public record GetProductQuery() : IQuery<GetProductsResult>;
+
+//La Respuesta de Nuestra solictud
+public record GetProductsResult(IEnumerable<Product> Products);
+
+//la mejor pratica separar de quey y comand
+//es una clase que solo se puede acceder desde este archivo
+//el Primer parametro despues del Gneric es lo que necesita
+//la interface de Query y su resultado
+internal class GetProductsQueryHandler
+    //Inyectamos las dependencias con el constructor primario
+    (IDocumentSession session,ILogger<GetProductsQueryHandler> logger)
+    //Es necesario implentar la interfaz
+    : IQueryHandler<GetProductQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
+    {
+        //mandar logs para mandar informacion de que esta pasando
+        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}",query);
+
+        //definimos la consulta
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        //Retornamos un resultado proveniente de nuestra Firma
+        //Se Usa new para crar un objeto en memoria
+        return new GetProductsResult(products);
+        
+    }
+}
+
